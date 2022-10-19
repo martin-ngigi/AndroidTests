5f0a6bb67d8d0bb32fa74cac8c494654b4ea73a6
martin-ngigi <martinwainaina001@gmail.com>
2022-10-19 14:40:18 +0300

    JUnit 4 Test Success

---
+++ app/src/main/java/com/example/androidtests/Models/Car.java
@@ -0,0 +1,17 @@
+package com.example.androidtests.Models;
+
+public class Car {
+    private String make;
+
+    public Car(String make) {
+        this.make = make;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+}
